79300bab8d11901f36f6e3f6b3446aa941c55f82
Triệu Manh Tùng <50283443+TrieuTung@users.noreply.github.com>
2024-05-04 16:38:39 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,12 @@
-resource "null_resource" "example" {}
+resource "github_branch" "branch" {
+  repository  = "test_atlantis"
+  branch      = "atlantis"
+}
+
+resource "github_repository_file" "file" {
+  repository           = github_branch.branch.repository
+  branch               = github_branch.branch.branch
+  file                 = "hello-world.txt"
+  content              = "Hello World"
+  overwrite_on_create  = true
+}
